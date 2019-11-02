455e97baee69f7e09d735d22aea68a50cca82a49
jan-wong <543050768@qq.com>
2019-11-02 13:56:55 +0800

    add richEditor.ts

---
+++ src/core/richEditor.ts
@@ -0,0 +1,41 @@
+/**
+ * 富文本编辑器类
+ */
+
+import Toolbar from './toolbar';
+import Control from './control';
+import Editor from './editor';
+import svgs from './svgs';
+
+// 参数接口
+interface Options {
+  el: HTMLElement;
+}
+
+class RichEditor {
+  el: HTMLElement;
+  svgs: typeof svgs; // 图标管理器
+  toolbar: Toolbar; // 工具栏管理器
+  control: Control; // 控件管理器
+  editor: Editor; // 编辑器区域
+  
+  constructor(options: Options) {
+    if (!options.el) throw Error('el option is must');
+
+    this.el = options.el;
+    this.svgs = svgs;
+    this.control = new Control();
+    this.toolbar = new Toolbar(this.el);
+    this.editor = new Editor(this.el);
+
+    // 注册内置工具栏插件
+    this.toolbar.registerPlugins(this);
+  }
+
+  // 对外提供增加svg的接口
+  addIcon(name: string, svg: string) {
+    this.svgs[name] = svg;
+  }
+}
+
+export default RichEditor;
