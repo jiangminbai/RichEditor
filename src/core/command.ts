8e1537c1523ddccd46dda91bec8ee6fd766ff034
jan-wong <543050768@qq.com>
2019-11-09 23:12:02 +0800

    封装document.execCommand

---
+++ src/core/command.ts
@@ -0,0 +1,25 @@
+/**
+ * command: document.execCommand存在一些缺陷，此函数进行一些修复
+ */
+import Editor from './editor';
+
+export default function(editor: Editor, commandName: string, showDefaultUI: boolean, ...args: string[]) {
+  if (commandName === 'link') {
+    const startContainer = editor.range.startContainer;
+    const parentNode = (<HTMLElement>startContainer.parentNode);
+    if (parentNode.tagName === 'A') {
+      parentNode.setAttribute('url', args[0]);
+      parentNode.setAttribute('title', args[2]);
+      parentNode.textContent = args[1];
+    } else {
+      const a = document.createElement('a');
+      a.setAttribute('url', args[0]);
+      a.setAttribute('title', args[2]);
+      a.textContent = args[1];
+      parentNode.insertBefore(a, startContainer);
+    }
+    return;
+  }
+
+  document.execCommand(commandName, showDefaultUI, args[0]);
+}
